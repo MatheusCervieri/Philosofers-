02813519c23c4256fd46b273565d730fd22cb5e4
MatheusCervieri <mathcervieri@gmail.com>
2022-08-19 20:55:59 -0300

    Args check

--- philo/main.c
+++ philo/main.c
@@ -6,16 +6,16 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 10:54:25 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/19 18:43:14 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/19 20:52:28 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 /*
-	0- Tratar os erros - malloc, número de argumentos...
+	0- Tratar os erros - malloc[x], número de argumentos...
 	1- Lidar com a exceção de apenas um filósofo.
-	2- Lidar com o problema que os filosofos estão pegando o mesmo gargo --> posso criar um array que armazena se o garfo já foi pego ou não.
+	2- Lidar com o problema que os filosofos estão pegando o mesmo garfo --> posso criar um array que armazena se o garfo já foi pego ou não.
 	3- Check how the death of the philosofer is verified. (Ver a régua).
 	4- Fazer os testes da régua.
 	5- Fazer os testes. 
@@ -25,18 +25,22 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc != 0)
+	if (argc != 5 && argc != 6)
 	{
-		if(argc == 6)
+		write(2, "Invalid Number of Arguments\n", 28);
+	}
+	else
+	{
+		if (argc == 6) 
 		{
 			data.n_eat = ft_atoi(argv[5]);
 			data.five_parameter = 1;
 		}
-		else 
+		else
 			data.five_parameter = 0;
 		initialization(&data, argv);
 		create_thread(&data);
+		exit_handle(&data);
 	}
-	exit_handle(&data);
 	return (0);
 }
