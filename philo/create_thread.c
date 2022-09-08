6b76fd6e560b0a46fea25b00ceaf68971bff9c24
MatheusCervieri <mathcervieri@gmail.com>
2022-09-08 10:44:50 -0300

    Today was a good day :D

--- philo/create_thread.c
+++ philo/create_thread.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 15:58:04 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/09/08 10:22:25 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/09/08 10:44:04 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,22 @@ int take_forks(t_philosofer *philosofer)
 	pthread_mutex_lock(&(data->forks_m[philosofer->right_fork]));
 	if (data->forks[philosofer->left_fork] == 0 && data->forks[philosofer->right_fork] == 0)
 	{
+		pthread_mutex_lock(data->eat_m);
 		data->forks[philosofer->left_fork] = 1;
 		print_stage_id(data, philosofer->nb, "has taken a fork", philosofer->left_fork);
 		data->forks[philosofer->right_fork] = 1;
 		print_stage_id(data, philosofer->nb, "has taken a fork", philosofer->right_fork);
 		philosofer->last_meal = get_time();
 		print_stage(data, philosofer->nb, "is eating");
+		pthread_mutex_unlock(data->eat_m);
 		philosofer->eats = philosofer->eats + 1;
 		usleep(data->t_eat * 1000);
 		data->forks[philosofer->left_fork] = 0;
 		data->forks[philosofer->right_fork] = 0;
 		pthread_mutex_unlock(&(data->forks_m[philosofer->left_fork]));
 		pthread_mutex_unlock(&(data->forks_m[philosofer->right_fork]));
+		if (*(data->loop) == 0)
+			return (1);
 		print_stage(data, philosofer->nb, "is sleeping");
 		usleep(data->t_sleep * 1000);
 		if (*(data->loop) == 0)
@@ -88,6 +92,7 @@ void death_checker(t_data *data)
 		i++;
 	}
 	}
+
 }
 
 void	create_thread(t_data *data)
